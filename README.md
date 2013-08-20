d5348e46a5f1c2f6ab24904b70c8d97b009ddd5c
munjeli <f3nn3lpoll3n@gmail.com>
2013-08-20 00:43:50 -0700

    beginning d3

---
+++ README.md
@@ -0,0 +1,3 @@
+Round two: I'm trying to make a democracule in d3 because I got bogged down in paper.js (which was great for some sorts of interactive drawing and scribble toys, but bit it on dynamically rescaling shapes). 
+
+
---
+++ d3/democracule-d3.js
@@ -0,0 +1,12 @@
+	window.onload=function(){
+		   
+			    
+           /*var citWork = d3.select("div.democraDiv").append("circle")
+	           				.attr({
+	           					'r': 50,
+	           					'cx':200,
+	           					'cy':50,
+	           					'stroke':'#F7853A',
+	           					'stroke-width': '10px'
+	           				});  */     
+           };
